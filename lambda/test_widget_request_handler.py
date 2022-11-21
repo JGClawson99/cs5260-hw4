a6d2037e5a04b85e9446cfa2d6398ec98a92fc26
Jacob Clawson <JacobClawson99@gmail.com>
2022-11-21 14:57:30 -0700

    improved lambda function

--- lambda/test_widget_request_handler.py
+++ lambda/test_widget_request_handler.py
@@ -8,27 +8,26 @@ class TestWidgetRequestHandler(unittest.TestCase):
     @mock_lambda
     def test_valid_create_widget_request(self):
         widget_request = {"message": "{\"type\":\"create\",\"requestId\":\"9ca0d18a-57ab-4ac6-89dc-146f092ea9fe\",\"widgetId\":\"ad0bb9e1-28e9-46e0-ad08-8192f4d3b6c6\",\"owner\":\"JohnJones\",\"label\":\"QQGRLNZY\",\"description\":\"JLVIEOHPQXKDXKPHOHFOXNKSYDEWRNEQWMPVPVHZVJCHCUIIWSRXITPWOKTMHULMVUNWGRREQYPQYO\",\"otherAttributes\":[{\"name\":\"size\",\"value\":\"926\"},{\"name\":\"height\",\"value\":\"828\"},{\"name\":\"height-unit\",\"value\":\"cm\"},{\"name\":\"length-unit\",\"value\":\"cm\"},{\"name\":\"rating\",\"value\":\"1.6420901\"}]}"}
-
-        assert lambda_handler(widget_request) == [False, "error sending message"]
+        
+        assert lambda_handler(widget_request)['body'] == "400: error sending message"
 
     @mock_lambda
     def test_valid_update_widget_request(self):
         widget_request = {"message": "{\"type\":\"update\",\"requestId\":\"d61c3e72-1a66-4cfa-9162-56712e4580d8\",\"widgetId\":\"6984abeb-5b24-42eb-93cc-3a5bef6b4b8a\",\"owner\":\"MaryMatthews\",\"description\":\"PUMMCL\",\"otherAttributes\":[{\"name\":\"size\",\"value\":\"745\"},{\"name\":\"size-unit\",\"value\":\"cm\"},{\"name\":\"height\",\"value\":\"879\"},{\"name\":\"height-unit\",\"value\":\"cm\"},{\"name\":\"width-unit\",\"value\":\"cm\"},{\"name\":\"length\",\"value\":\"793\"},{\"name\":\"price\",\"value\":\"50.96\"},{\"name\":\"quantity\",\"value\":\"311\"},{\"name\":\"note\",\"value\":\"MYEVVLRLAWVRZTQIMWRTJFDZTSJNJTWXQBFXOBABMNGJDCWRJMAGVYSWWAPYWDCHSDKFAURWSBHGABSMVKRLQZKXEXJLNXZU\"}]}"}
 
-        assert lambda_handler(widget_request) == [False, "error sending message"]
+        assert lambda_handler(widget_request)['body'] == "400: error sending message"
 
     @mock_lambda
     def test_valid_delete_widget_request(self):
         widget_request = {"message": "{\"type\":\"delete\",\"requestId\":\"9205fcb9-7c74-4db1-8054-33bdd70ed0e8\",\"widgetId\":\"6984abeb-5b24-42eb-93cc-3a5bef6b4b8a\",\"owner\":\"MaryMatthews\"}"}
 
-        assert lambda_handler(widget_request) == [False, "error sending message"]
+        assert lambda_handler(widget_request)['body'] == "400: error sending message"
 
     @mock_lambda
     def test_invalid_widget_request(self):
         widget_request = {"message": "{\"explode\":\"delete\",\"requestId\":\"9205fcb9-7c74-4db1-8054-33bdd70ed0e8\",\"widgetId\":\"6984abeb-5b24-42eb-93cc-3a5bef6b4b8a\",\"owner\":\"MaryMatthews\"}"}
-
-        assert lambda_handler(widget_request)[0] == False
-
+        
+        assert lambda_handler(widget_request)['body'] == "400: type not in request"
 
 if __name__ == '__main__':
     unittest.main()
--- lambda/widget_request_handler.py
+++ lambda/widget_request_handler.py
@@ -4,26 +4,52 @@ import boto3
 queue_url = "https://sqs.us-east-1.amazonaws.com/839482323059/cs5260-requests"
 
 def lambda_handler(event, context=None):
-    if not 'message' in event:
-        return [False, "no message found"]
+    if 'message' in event:
+        body = [False, "no message found"]
         
-    request = event['message']
-    try:
-        json_request = json.dumps(request)
-        json.loads(json_request)
-        if 'type' not in json_request:
-            return [False, "type not in request"]
-        elif 'requestId' not in json_request:
-            return [False, "requestId not in request"]
-        elif 'widgetId' not in json_request:
-            return [False, "widgetId not in request"]
-        elif 'owner' not in json_request:
-            return [False, "owner not in request"]
-        else:
-            try:
-                return [True, boto3.client('sqs').send_message(QueueUrl=queue_url, MessageBody=json_request)]
-            except:
-                return [False, "error sending message"]
-            
-    except:
-        return [False, "invalid json"]
+        request = event['message']
+        try:
+            json_request = json.dumps(request)
+            json.loads(json_request)
+            if 'type' not in json_request:
+                return error("type not in request")
+            elif 'requestId' not in json_request:
+                return error("requestId not in request")
+            elif 'widgetId' not in json_request:
+                return error("widgetId not in request")
+            elif 'owner' not in json_request:
+                return error("owner not in request")
+            else:
+                try:
+                    return formatResponse(boto3.client('sqs').send_message(QueueUrl=queue_url, MessageBody=json_request))
+                except:
+                    return error("error sending message")
+        except:
+            return error("invalid format")
+    else:
+        return error("message not found")
+    
+def error(message):
+    return {
+            "statusCode": 400,
+            "headers": {
+                "Content-Type": "text/plain",
+                "x-amzn-ErrorType": 400
+            },
+            "isBase64Encoded": "false",
+            "body": "400: " + message
+        }
+    
+def formatResponse(body):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "isBase64Encoded": "false",
+        "multiValueHeaders": {
+            "X-Custom-Header": ["My value", "My other value"]
+        },
+        "body": body
+    }
+    
