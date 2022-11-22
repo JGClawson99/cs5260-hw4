b78aceebee321712c61b8d38538cc1370dd8a844
Jacob Clawson <JacobClawson99@gmail.com>
2022-11-21 17:41:54 -0700

    cleaned up code

--- lambda/widget_request_handler.py
+++ lambda/widget_request_handler.py
@@ -4,10 +4,8 @@ import boto3
 queue_url = "https://sqs.us-east-1.amazonaws.com/839482323059/cs5260-requests"
 
 def lambda_handler(event, context=None):
-    if 'message' in event:
-        body = [False, "no message found"]
-        
-        request = event['message']
+    if 'body' in event:
+        request = event['body']
         try:
             json_request = json.dumps(request)
             json.loads(json_request)
@@ -21,7 +19,8 @@ def lambda_handler(event, context=None):
                 return error("owner not in request")
             else:
                 try:
-                    return formatResponse(boto3.client('sqs').send_message(QueueUrl=queue_url, MessageBody=json_request))
+                    boto3.client('sqs').send_message(QueueUrl=queue_url, MessageBody=json_request)
+                    return formatResponse("success")
                 except:
                     return error("error sending message")
         except:
@@ -44,12 +43,12 @@ def formatResponse(body):
     return {
         "statusCode": 200,
         "headers": {
-            "Content-Type": "application/json"
+            "Content-Type": "text/plain"
         },
         "isBase64Encoded": "false",
         "multiValueHeaders": {
             "X-Custom-Header": ["My value", "My other value"]
         },
-        "body": body
+        "body": "200: " + body
     }
     
